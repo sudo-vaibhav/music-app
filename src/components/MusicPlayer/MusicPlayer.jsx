c90c1437c1b525832cd7f65ba521256659604fcf
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-03-27 02:33:37 +0530

    now play pause also works

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -9,6 +9,7 @@ import WaveSurfer from 'wavesurfer.js'
 const MusicPlayer = () => {
   const { songId } = useParams()
   const waveformRef = useRef(null)
+  const wavesurfer = useRef(null)
 
   const [{ playing, song, color }, setData] = useState({
     song: {
@@ -27,6 +28,7 @@ const MusicPlayer = () => {
       ...{ color, song },
       playing: !playing,
     })
+    wavesurfer.current.playPause()
   }
   useEffect(() => {
     ;(async () => {
@@ -39,41 +41,34 @@ const MusicPlayer = () => {
         vibrantColor = palette.Vibrant.hex
 
         if (waveformRef.current) {
-          const wavesurfer = WaveSurfer.create({
+          wavesurfer.current = WaveSurfer.create({
             container: waveformRef.current,
             // waveColor: colors.primary,
             waveColor: 'transparent',
             progressColor: 'white',
             cursorColor: 'white',
-            barWidth: 3,
-            barRadius: 3,
+            barWidth: 4,
+            barRadius: 10,
             responsive: true,
-            height: 150,
+            height: 75,
             // If true, normalize by the maximum peak instead of 1.0.
             normalize: true,
             // Use the PeakCache to improve rendering speed of large waveforms.
             partialRender: true,
           })
           console.log(song.song_url)
-          wavesurfer.load(song.song_url)
-          wavesurfer.on('ready', function () {
+          wavesurfer.current.load(song.song_url)
+          wavesurfer.current.on('ready', function () {
             // https://wavesurfer-js.org/docs/methods.html
-            wavesurfer.play()
-            // setPlay(true);
+            wavesurfer.current.play()
 
-            // make sure object stillavailable when file loaded
-            // if (wavesurfer.current) {
-            //   wavesurfer.current.setVolume(volume)
-            //   setVolume(volume)
-            // }
+            setData({
+              song,
+              color: vibrantColor,
+              playing: true,
+            })
           })
         }
-
-        setData({
-          song,
-          color: vibrantColor,
-          playing: true,
-        })
       })
     })()
   }, [songId])
@@ -84,9 +79,13 @@ const MusicPlayer = () => {
       <div style={{ background: color }} className="music-player__player-area">
         <h1>{song.name}</h1>
         <h4>{song.artist}</h4>
-        <div className="container" ref={waveformRef}></div>
+        <div className="container w-75" ref={waveformRef}></div>
         <div onClick={handlePlayPause}>
-          {playing ? <FeatherIcon icon="pause" /> : <FeatherIcon icon="play" />}
+          {playing ? (
+            <FeatherIcon icon="pause" size={36} />
+          ) : (
+            <FeatherIcon icon="play" size={36} />
+          )}
         </div>
       </div>
     </div>
